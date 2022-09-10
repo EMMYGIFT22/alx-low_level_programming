6dab1bc216f6fd5a13fda69665fc75a536e41119
EMMYGIFT22 <emmygift2003@gmail.com>
2022-09-10 02:30:40 -0700

    learning c programming

---
+++ c/0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  //string initialisation
+  char mystr[] = "programming is like building a multilingual puzzle";
+  
+  puts(mystr); //writing the string to stdout
+  
+  return 0;
+}
