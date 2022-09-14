435da91db642ffabecc607523dea48f7ffaff4e2
EMMYGIFT22 <emmygift2003@gmail.com>
2022-09-14 07:56:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * program to print alphabet
+ *
+ *Return Always 0.
+ */
+int main()
+{
+  void print_alphabet(void);
+  char i;
+  for(i= 'a'; i<= 'z'; i++){
+    putchar(i);
+  }
+  return 0;
+}
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *main - check the code
+ *main - check code
  *
- *Return: Always 0.
+ *Return Always 0.
  */
-int main (void)
+int main(void)
 {
   print_alphabet();
   return 0;
