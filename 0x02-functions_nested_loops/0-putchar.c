2dbfea1e69619c9506e720e6908d88b29bbf82d4
EMMYGIFT22 <emmygift2003@gmail.com>
2022-09-14 03:37:35 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/* more header goes there */
 
+/* betty style doc for function main goes there */
 int main()
 {
   printf("_putchar\n");
+  /* your code goes there */
   return 0;
 }
