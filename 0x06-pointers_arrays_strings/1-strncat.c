b1bd98fc57f69c72147458e52e096c5e1ad78483
EMMYGIFT22 <emmygift2003@gmail.com>
2022-09-21 04:59:10 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  char *_strcat(char *dest, char *src);
+    
+  printf("Hello\n");
+  printf("World!\n");
+  printf("Hello W\n");
+  printf("World!\n");
+  printf("Hello W\n");
+  printf("Hello WWorld!\n");
+  printf("World\n");
+  printf("Hello WWorld!\n");
+  return 0;
+}
+
